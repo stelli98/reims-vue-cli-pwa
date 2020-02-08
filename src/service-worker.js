c5f611270c63a2ed7f0d8d122e4dfb0e13ca1e98
stelli <stellitan98@gmail.com>
2020-02-08 08:11:17 +0700

    medical offline

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,53 +1,28 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.5.0/workbox-sw.js');
-
 if (workbox) {
   // workbox.core.setLogLevel(workbox.core.LOG_LEVELS.debug);
 
   workbox.precaching.precacheAndRoute(self.__precacheManifest);
 
-  // workbox.routing.registerNavigationRoute("/index.html");
+  workbox.routing.registerNavigationRoute("/index.html");
 
-  workbox.routing.registerRoute(
-    new RegExp("/api"),
-    new workbox.strategies.NetworkOnly()
-  );
-
-  // const bgSyncPlugin = new workbox.backgroundSync.Plugin("medicalsOffline", {
-  //   maxRetentionTime: 24 * 60 // Retry for max of 24 Hours
-  // });
-
-  // workbox.routing.registerRoute(
-  //   new RegExp("/api/medicals"),
-  //   workbox.strategies.networkOnly({
-  //     plugins: [bgSyncPlugin]
-  //   }),
-  //   "POST"
-  // );
-
-  const showNotification = () => {
-    self.registration.showNotification('Background sync success!', {
-      body: '🎉`🎉`🎉`'
-    });
-  };
-
-  const bgSyncPlugin = new workbox.backgroundSync.Plugin(
-    'medicalsOffline',
-    {
-      callbacks: {
-        queueDidReplay: showNotification
-        // other types of callbacks could go here
-      }
-    }
-  );
+  const queue = new workbox.backgroundSync.Queue("offlineMedicals");
 
   const networkWithBackgroundSync = new workbox.strategies.NetworkOnly({
-    plugins: [bgSyncPlugin],
+    plugins: [
+      {
+        fetchDidFail: async ({ request }) => {
+          console.log(request)
+          await queue.addRequest(request);
+        }
+      }
+    ]
   });
 
-  workbox.routing.registerRoute(
-    new RegExp("/api/medicals"),
+  const addEventRoute = new workbox.routing.Route(
+    ({ url }) => url.pathname === "/api/medicals",
     networkWithBackgroundSync,
-    'POST'
+    "POST"
   );
 
+  workbox.routing.registerRoute(addEventRoute);
 }
